76405fc2c9fc357f7c0b7bbce912f0c97cf5547f
asvn62 <asvn62@gmail.com>
2018-02-07 21:11:07 +0700

    update

---
+++ lesson13/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.review__slider').slick({
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa  fa-angle-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa   fa-angle-right"></i></button>'
+    });
+});
