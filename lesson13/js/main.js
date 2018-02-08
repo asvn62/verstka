ac2d655eb9d01404d23f3df8bf025c6ba126fa96
asvn62 <asvn62@gmail.com>
2018-02-08 20:22:56 +0700

    update

--- lesson13/js/main.js
+++ lesson13/js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('.review__slider').slick({
         arrows: true,
         prevArrow: '<button type="button" class="slick-prev"><i class="fa  fa-angle-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa   fa-angle-right"></i></button>'
+        nextArrow: '<button type="button" class="slick-next"><i class="fa   fa-angle-right"></i></button>',
+        dots: true
     });
 });
